2efb634488124b1d2343c271cbbe92bcbc2d1900
Marco <marco.escobar@ucb.edu.bo>
2022-10-03 08:17:50 -0400

    Portafolio 0.1

---
+++ src/botonArriba.js
@@ -0,0 +1,12 @@
+function irArriba (pxPantalla) {
+    window.addEventListener ('scroll', () => {
+        var scroll = document.documentElement.scrollTop;
+        var botonArriba = document.getElementById ('botonArriba') ;
+        if ( scroll > pxPantalla ) {
+            botonArriba.style.right = 30 + "px" ;
+        }else {                    
+            botonArriba.style.right = -100 + "px" ;
+        }
+    })
+}
+irArriba(500);
